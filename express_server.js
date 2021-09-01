33d2b02a79a074e541ec4257ea94a1784e953a47
magotts <techybet@gmail.com>
2021-08-31 20:58:57 -0400

    ...

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+const cookieParser = require('cookie-parser');
+
 
 app.set("view engine", "ejs");
 
@@ -20,7 +22,9 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log(res.cookies);
+  const templateVars = { urls: urlDatabase};
+   // username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -63,7 +67,15 @@ app.post("/urls/:shortURL", (req, res) => { /// edit the url
   const shortURL = req.params.shortURL;
   let editedURL = req.body.edited;
   urlDatabase[shortURL] = editedURL;
-  res.redirect(`/urls/`);
+  res.redirect("/urls/");
+});
+
+app.post("/login", (req, res) => { /// cookie username
+  let username = req.body.username;
+  res.cookie("username", username);
+  console.log(req.cookies);
+  console.log("cookie is", username);
+  res.redirect("/urls");
 });
 
 
