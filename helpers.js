b2b94d7336f354733d937f0b1807f1f32dcd45c5
magotts <techybet@gmail.com>
2021-09-02 16:40:20 -0400

    creating new helpers.js file

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const getUserByEmail = (email, users) => {
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return false;
+};
+
+module.exports = { getUserByEmail };
